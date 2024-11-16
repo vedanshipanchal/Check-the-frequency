364d93fdb597be3f307a7af888f17a4dabcfba9d
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-16 17:00:29 +0000

    Get rid of the duplicates

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-test = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+student_data = {
 
-print("The original dict is:",test)
+'stu1': {'name':'Tasheen', 'grade':5, 'id':1,  'subject':["Eng","python"]},
+'stu2': {'name':'Vedanshi', 'grade':5, 'id':2,  'subject':["Eng","python"]},
+'stu3': {'name':'Chiamalite', 'grade':5, 'id':3,  'subject':["Eng","python"]},
+'stu4': {'name':'Vedanshi', 'grade':5, 'id':2,  'subject':["Eng","python"]},
 
+}
 
-x = 2
+result = {}
 
-
-count = 0
-for key in test:
-    if test[key]== x:
-        count = count+1
-
-print("Frequency is:",count)
+for key,value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+print(result)
